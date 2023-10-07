3d2f11ea27bef2f55a710bcc16e6c7b8478661a8
Hafedh Ben Haj Ali <70435711+hafedhBel@users.noreply.github.com>
2023-10-07 07:40:04 +0100

    Add files via upload

--- game.js
+++ game.js
@@ -1,29 +1,64 @@
-var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
-userClickedPattern = [];
+var userClickedPattern = [];
+
 var started = false;
 var level = 0;
 
-$(document).keypress(function () {
+$(document).keypress(function() {
   if (!started) {
-
-    //3. The h1 title starts out saying "Press A Key to Start", when the game has started, change this to say "Level 0".
     $("#level-title").text("Level " + level);
     nextSequence();
     started = true;
   }
 });
 
+$(".btn").click(function() {
 
-$(".btn").click(function () {
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
-  var audio = new Audio("sounds/" + userChosenColour + ".mp3");
-  audio.play();
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+
+  checkAnswer(userClickedPattern.length-1);
 });
 
+
+function checkAnswer(currentLevel) {
+
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+
+      console.log("success");
+
+      if (userClickedPattern.length === gamePattern.length){
+        setTimeout(function () {
+          nextSequence();
+        }, 1000);
+      }
+
+    } else {
+
+      console.log("wrong");
+
+      //1. In the sounds folder, there is a sound called wrong.mp3, play this sound if the user got one of the answers wrong.
+      playSound("wrong");
+
+      //2. In the styles.css file, there is a class called "game-over", apply this class to the body of the website when the user gets one of the answers wrong and then remove it after 200 milliseconds.
+      $("body").addClass("game-over");
+      setTimeout(function () {
+        $("body").removeClass("game-over");
+      }, 200);
+
+      //3. Change the h1 title to say "Game Over, Press Any Key to Restart" if the user got the answer wrong.
+      $("#level-title").text("Game Over, Press Any Key to Restart");
+    }
+
+}
+
 function nextSequence() {
+
+  userClickedPattern = [];
   level++;
   $("#level-title").text("Level " + level);
 
@@ -31,24 +66,27 @@ function nextSequence() {
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
 
-  //1. Use jQuery to select the button with the same id as the randomChosenColour
-  //2. Use Google/Stackoverflow to figure out how you can use jQuery to animate a flash to the button selected in step 1.
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-
-  //3. Use Google/Stackoverflow to figure out how you can use Javascript to play the sound for the button colour selected in step 1.
-  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-  audio.play();
+  playSound(randomChosenColour);
 }
+
 function playSound(name) {
   var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
 }
-function animatePress(currentColour) {
+
+function animatePress(currentColor) {
   $("#" + currentColor).addClass("pressed");
   setTimeout(function () {
-    ("#" + currentColour).removeClass("pressed");
+    $("#" + currentColor).removeClass("pressed");
   }, 100);
 }
-$(document).on('keypress');
+function startOver(){
+  level = 0 ;
+  gamePattern = [];
+  started = false ;
+}
+
+
 
 
